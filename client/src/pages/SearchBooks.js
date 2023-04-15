9fd9b2a6ad52b210f3df71642e083eb2d6d666e7
LucasBielinski <115428815+LucasBielinski@users.noreply.github.com>
2023-04-15 16:00:11 -0400

    fixed search books

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -67,13 +67,10 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
+      const { data } = await saveBook({
+        variables: { bookToSave, token },
+      });
 
-      if (!response.ok) {
-        throw new Error("something went wrong!");
-      }
-
-      // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
     } catch (err) {
       console.error(err);
